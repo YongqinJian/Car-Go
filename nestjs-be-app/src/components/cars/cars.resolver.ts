76128acfc2f7fe0104c7afe052fab4ce4029ebaf
Yongqin Jian <32945661+YongqinJian@users.noreply.github.com>
2022-01-28 16:17:28 -0700

    Creating Car Entity and Fetching Cars from Database

--- nestjs-be-app/src/components/cars/cars.resolver.ts
+++ nestjs-be-app/src/components/cars/cars.resolver.ts
@@ -1,5 +1,7 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CarsService } from "./cars.service";
+import { NewCarInput } from "./dto/new-car.input";
+import { Car } from "./entities/cars";
 
 @Resolver()
 
@@ -9,10 +11,17 @@ export class CarsResolver {
 
     }
 
-    @Query(returns => String)
+    @Query(returns => [Car])
     // exact name when you going to use the query
-    public async cars(){
-        return 'Hello and Welcome to CarGo shop!'
+    public async cars(): Promise<Car[]> {
+        return await this.carsService.getAllCars().catch((err) => {
+            throw err;
+        });
+    }
+
+    @Mutation(returns => Car)
+    public async addNewCar(@Args("newCarData") newCarInput: NewCarInput ): Promise<Car>{
+
     }
 
 }
---
+++ nestjs-be-app/src/components/cars/dto/new-car.input.ts
@@ -0,0 +1,27 @@
+import { Field, InputType, Int } from "@nestjs/graphql";
+
+@InputType()
+export class NewCarInput {
+
+    @Field()
+    name: string;
+
+    @Field(type => Int)
+    monthlyPrice: number;
+
+    @Field(type => Int)
+    dailyPrice: number;
+
+    @Field()
+    gas: string;
+
+    @Field()
+    mileage: string;
+
+    @Field()
+    gearType: string;
+
+    @Field()
+    thubmnailUrl: string;
+
+}
